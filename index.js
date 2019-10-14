8e71541e8f8f6b13f344ccc1e112324537d8f02c
Asthriona <im@asthriona.com>
2019-10-14 11:02:00 +0200

    The Base

---
+++ index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+var port = 3000;
+
+function onRequest(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data){
+        if(error){
+            res.writeHead(500);
+            res.write('500, Internal Error.')
+        }else{
+            res.write(data);
+        }
+        res.end();
+    })
+}
+http.createServer(onRequest).listen(port)
